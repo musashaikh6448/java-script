4e3066ba0c702874619913e6f4c4e0ea447971cf
shaikhmusa6448@gmail.com <shaikhmusa6448@gmail.com>
2024-07-16 16:31:01 +0530

    changes

--- day7/prc.js
+++ day7/prc.js
@@ -1,11 +1,79 @@
-let a =10 
-let b =20
-//let c =30
+// let a =10 
+// let b =20
+//let c =30                           (//)
 
 /*let d =40
-let e =20
+let e =20                            (/+*)  (*+/)
 let f =10*/
 
 
+// let c =30
+// let d =40
+// let e =20                         (ctrl+/)
+// let f =10
 
+
+// console.log(a)
+
+
+let a;                                    //undefined
 console.log(a)
+
+let x=10;                                 //number
+console.log(x)
+
+let name= "shaikh"                        //string a-z, A-Z, numbers, special charachters
+console.log(name)
+
+let email="musashaikh6448@gmail.com"      //double cout
+console.log(email)
+
+let fname='shaikh'                         //single cout
+console.log(fname)
+
+let lname=`musa`                          //back tick     //temlete literial
+console.log(lname)
+
+let hadlunch =false;                     //boolean true ,false
+console.log(hadlunch)
+
+let flag=true;                           //boolean
+console.log(flag)
+
+let n=null;                              //null empty
+console.log(n)
+
+let arr=[10,20,30];                       //array is special variable which is used for storing multiple value
+console.log(arr)
+
+let obj={name:"sonu", age:20}            //object : properties and function
+console.log(obj)
+
+
+// ******************************************************
+
+
+
+let name1= "musa"
+console.log(name1)
+
+let x1=10
+console.log(x1)
+
+let x$$_name="shaikh"
+console.log(x$$_name)
+
+let $=10
+console.log($)
+
+let $$4x=20
+console.log($$4x)
+
+let _$1x=40
+console.log(_$1x)
+
+let p=30
+console.log(p)
+
+let o=4
+console.log(p)
